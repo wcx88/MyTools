52927f95b8594bf35172a752163f3fe6ad023784
wcx88 <wcx.8@163.com>
2017-08-28 15:44:49 +0800

    add mov

--- moveFile/1.moveFileByDate.bat
+++ moveFile/1.moveFileByDate.bat
@@ -1,24 +1,31 @@
 @echo off
 setlocal enabledelayedexpansion
 set pic_dir=".\"
-for /f %%i in ('dir /s /b %pic_dir%') do (
-	set str_date=%%~ti
-	set str_path=%%~nxi
-	if not "!str_path!" == "1.moveFileByDate.bat" (
-		set str_date=!str_date:~0,10!
-		set str_date=!str_date:/=_!
-		rem echo !str_date!
-		if not exist !str_date! (
-			md !str_date!
+for /f %%i in ('dir /a-d-s /b %pic_dir%') do (
+	set "str_date=%%~ti"
+	set "str_path=%%~nxi"
+	set "str_ext=%%~xi"
+	rem echo !str_date!    !str_path!
+	if not "!str_date!" == "" (
+	if not "!str_ext!" == ".bat" (
+		set str_dir=!str_date:~0,10!
+		set str_dir=!str_dir:/=_!
+		
+		if "!str_ext!" == ".MOV" (
+			set str_dir=!str_dir!\สำฦต
+			)
+		if not exist !str_dir! (
+			md !str_dir!
 			)
 
-		echo !str_path!
 		
-		rem copy !str_path! !str_date!\
-		move !str_path! !str_date!\
+		echo !str_dir!    !str_path!
+		rem copy !str_path! !str_dir!\
+		move "!str_path!" "!str_dir!\"
 
 		)
 
+	)
 	)
 
 pause
